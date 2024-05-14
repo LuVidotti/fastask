100af0132f5e9ba786609d022ad0617ba0f7fd1e
Luís Felipe Vidotti <luisfelipealmeida66@gmail.com>
2024-05-13 21:41:04 -0300

    inserindo tela de login

--- fastask/src/pages/Login/index.jsx
+++ fastask/src/pages/Login/index.jsx
@@ -1,7 +1,26 @@
+import { Link } from "react-router-dom";
+import "./Login.css";
+
 function Login() {
     return(
-        <div>
-            <h1>Tela de login</h1>
+        <div className="login-container">
+            <div className="login-container__card">
+                <h1>Login</h1>
+                <form>
+                    <label htmlFor="email">E-mail</label>
+                    <input type="email" id="email" placeholder="Digite seu e-mail"/>
+
+                    <label htmlFor="senha">Senha</label>
+                    <input type="password" placeholder="Digite sua senha"/>
+
+                    <button type="submit">Entrar</button>
+
+                    <div className="criar-conta-area">
+                        <p>Ainda não tem uma conta?</p>
+                        <Link className="criar-conta__link">Criar conta</Link>
+                    </div>  
+                </form>
+            </div>
         </div>
     )
 }
