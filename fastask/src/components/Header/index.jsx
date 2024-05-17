3f604811101e68ae45dc6183ada3f431346b34f4
Luís Felipe Vidotti <luisfelipealmeida66@gmail.com>
2024-05-17 03:26:08 -0300

    implementando apis no header e em login

--- fastask/src/components/Header/index.jsx
+++ fastask/src/components/Header/index.jsx
@@ -1,12 +1,13 @@
 import Logo from "../Logo";
 import "./Header.css";
-import Perfil from "../../assets/foto perfil.jpeg";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 function Header() {
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState({});
+    const [foto, setFoto] = useState("");
     const urlApi = "http://localhost:3000";
     const navegar = useNavigate();
 
@@ -16,20 +17,28 @@ function Header() {
             headers: {
                 Authorization: token
             }
-        }).then((user) => {
-            console.log(user);
+        }).then((resposta) => {
+            setUser(resposta.data)
+            setFoto(resposta.data.fotoPerfil.replace(/uploads\\/g, "").replace(/\\/g, "/"));
         }).catch((erro) => {
             console.log(erro);
-            navegar("/login");
+            toast.error(erro.response.data.message);
+            navegar('/login');
         })
     }, [user])
+
+    function sair() {
+        localStorage.removeItem("token");
+        setUser({});
+    }
 
     return (
         <header className="cabecalho">
             <Logo />
             <div className="perfil-div">
-                <h4>Luisinho</h4>
-                <img src={Perfil} alt="Foto de perfil do usuario" />
+                <h4>{user.nome}</h4>
+                <button onClick={() => sair()}>Sair</button>
+                <img src={`${urlApi}/usuarios/imagem/${foto}`} alt="Foto de perfil do usuario" />
             </div>
         </header>
     )
--- fastask/src/pages/Login/index.jsx
+++ fastask/src/pages/Login/index.jsx
@@ -1,17 +1,45 @@
 import { Link } from "react-router-dom";
 import "./Login.css";
+import { toast } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+    const urlApi = "http://localhost:3000";
+    const navegar = useNavigate();
+
+    function fazerLogin(e) {
+        e.preventDefault();
+
+        axios.post(`${urlApi}/usuarios/login`, {
+            email: email,
+            senha: senha
+        }).then((resposta) => {
+            console.log(resposta)
+            toast.success(resposta.data.message);
+            const token = resposta.data.token;
+            localStorage.setItem("token", token);
+            navegar("/");
+        }).catch((erro) => {
+            console.log(erro)
+            toast.error(erro.response.data.message);
+        })
+    }
+
     return(
         <div className="login-container">
             <div className="login-container__card">
                 <h1>Login</h1>
-                <form>
+                <form onSubmit={(e) => fazerLogin(e)}>
                     <label htmlFor="email">E-mail</label>
-                    <input type="email" id="email" placeholder="Digite seu e-mail"/>
+                    <input type="email" id="email" placeholder="Digite seu e-mail" onChange={(e) => setEmail(e.target.value)}/>
 
                     <label htmlFor="senha">Senha</label>
-                    <input type="password" placeholder="Digite sua senha"/>
+                    <input type="password" placeholder="Digite sua senha" onChange={(e) => setSenha(e.target.value)}/>
 
                     <button type="submit">Entrar</button>
 
