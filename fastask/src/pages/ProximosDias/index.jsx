464cdee3ece67a6b58d74d2b8ee4cb6ca99da29e
Luís Felipe Vidotti <luisfelipealmeida66@gmail.com>
2024-05-22 00:27:44 -0300

    integrando API de tarefas na pagina Proximos Dias

--- fastask/src/pages/ProximosDias/index.jsx
+++ fastask/src/pages/ProximosDias/index.jsx
@@ -2,8 +2,68 @@ import CategoriaItem from "../../components/CategoriaItem";
 import Form from "../../components/Form";
 import Header from "../../components/Header";
 import Nav from "../../components/Nav";
+import { toast } from "react-toastify";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function ProximosDias() {
+    const [tarefa, setTarefa] = useState("");
+    const [tarefas, setTarefas] = useState([]);
+    const urlApi = "http://localhost:3000";
+
+    function adicionarTarefa(e) {
+        e.preventDefault();
+        const token = localStorage.getItem("token");
+        
+        axios.post(`${urlApi}/tarefas/proximos-dias`, {
+            descricao: tarefa
+        }, {
+            headers: {
+                Authorization: token
+            }
+        }).then((resposta) => {
+            console.log(resposta);
+            toast.success(resposta.data.message);
+            setTarefa("");
+        }).catch((erro) => {
+            console.log(erro);
+            toast.error("Erro ao adicionar tarefa");
+        })
+    }
+
+    function deletarTarefa(id) {
+        const token = localStorage.getItem("token");
+
+        axios.delete(`${urlApi}/tarefas/${id}`, {
+            headers: {
+                Authorization: token
+            }
+        }).then((resposta) => {
+            toast.success(resposta.data.message);
+        }).catch((erro) => {
+            console.log(erro);
+            toast.error("Erro ao deletar tarefa");
+        })
+    }
+
+    useEffect(() => {
+        function pegarTarefas() {
+            const token = localStorage.getItem("token");
+
+            axios.get(`${urlApi}/tarefas/proximos-dias`, {
+                headers: {
+                    Authorization: token
+                }
+            }).then((resposta) => {
+                setTarefas(resposta.data);
+            }).catch((erro) => {
+                console.log(erro);
+            })
+        }
+
+        pegarTarefas();
+    }, [tarefas]);
+
     return (
         <div>
             <Header />
@@ -11,10 +71,13 @@ function ProximosDias() {
                 <Nav />
                 <div className="principal">
                     <h1>Próximos 7 dias</h1>
-                    <Form />
+                    <Form inputValor={tarefa} setTarefa={setTarefa} adicionarTarefa={adicionarTarefa} />
                     <div className="tarefas">
-                        <CategoriaItem titulo="Buscar a lolozinha"/>
-                        <CategoriaItem titulo="Comprar neosoro"/>
+                        {
+                            tarefas.map((item) => {
+                                return <CategoriaItem key={item._id} titulo={item.descricao} id={item._id} deletarTarefa={deletarTarefa}/>
+                            })
+                        }
                     </div>
                 </div>
             </div>
